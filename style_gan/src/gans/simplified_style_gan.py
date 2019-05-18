790196bbbef38eae176c4daace40776c147092e8
dragonmeteor <dragonmeteor@gmail.com>
2019-05-19 00:23:37 +0900

    more work

--- style_gan/src/gans/simplified_style_gan.py
+++ style_gan/src/gans/simplified_style_gan.py
@@ -53,7 +53,7 @@ def AdaIN(input: torch.Tensor, new_mean: torch.Tensor, new_std: torch.Tensor, ep
     input_std = (input_flattened.var(dim=2, keepdim=True) + epsilon).sqrt()
     input_normalized = (input_flattened - input_mean) / input_std
 
-    return (input_normalized * new_std.view(1, c, 1) + new_mean.view(1, c, 1)).view(n, c, h, w)
+    return (input_normalized * new_std.view(n, c, 1) + new_mean.view(n, c, 1)).view(n, c, h, w)
 
 
 class GeneratorBlock(GanModule):
@@ -101,31 +101,31 @@ class GeneratorBlock(GanModule):
                 noise_1: torch.Tensor = None,
                 noise_2: torch.Tensor = None):
         upsampled_image = self.upsample(input_image)
-
-        n = upsampled_image.shape[0]
-        c = upsampled_image.shape[1]
-        h = upsampled_image.shape[2]
-        w = upsampled_image.shape[3]
-
-        conv_1_image = F.leaky_relu(self.conv_1(upsampled_image), negative_slope=self.leaky_relu_negative_slope)
-
-        if noise_1 is None:
-            noise_1 = torch.randn(n, 1, h, w).expand(n, c, h, w) * self.noise_1_factor
-        else:
-            noise_1 = noise_1.expand(n, c, h, w) * self.noise_1_factor
-
-        noise_1_image = conv_1_image + noise_1
+        conv_1_image = self.convolve_1(upsampled_image)
+        noise_1_image = conv_1_image + self.create_noise(conv_1_image, self.noise_1_factor, noise_1)
         adain_1_image = AdaIN(noise_1_image, self.weight_to_new_mean_1(weight), self.weight_to_new_std_1(weight))
-        conv_2_image = F.leaky_relu(self.conv_2(adain_1_image), negative_slope=self.leaky_relu_negative_slope)
-
-        if noise_2 is None:
-            noise_2 = torch.randn(n, 1, h, w).expand(n, c, h, w) * self.noise_2_factor
-        else:
-            noise_2 = noise_2.expand(n, c, h, w) * self.noise_2_factor
-
-        noise_2_image = conv_2_image + noise_2
+        conv_2_image = self.convolve_2(adain_1_image)
+        noise_2_image = conv_2_image + self.create_noise(conv_2_image, self.noise_2_factor, noise_2)
         adain_2_image = AdaIN(noise_2_image, self.weight_to_new_mean_2(weight), self.weight_to_new_std_2(weight))
         return adain_2_image
+
+    def convolve_1(self, input_image: torch.Tensor):
+        return F.leaky_relu(self.conv_1(input_image), negative_slope=self.leaky_relu_negative_slope)
+
+    def convolve_2(self, input_image: torch.Tensor):
+        return F.leaky_relu(self.conv_2(input_image), negative_slope=self.leaky_relu_negative_slope)
+
+    def create_noise(self, input_image: torch.Tensor, noise_factor: torch.Tensor, input_noise: torch.Tensor = None):
+        n = input_image.shape[0]
+        c = input_image.shape[1]
+        h = input_image.shape[2]
+        w = input_image.shape[3]
+
+        if input_noise is None:
+            raw_noise = torch.randn(n, 1, h, w).expand(n, c, h, w)
+        else:
+            raw_noise = input_noise.view(1,1,h,w).expand(n, c, h, w)
+        return raw_noise * noise_factor
 
     def initialize(self):
         pass
--- style_gan/src/test/gans/test_simplififed_pggan.py
+++ style_gan/src/test/gans/test_simplififed_pggan.py
@@ -1,7 +1,7 @@
 import torch
 import unittest
 
-from gans.simplified_style_gan import AdaIN
+from gans.simplified_style_gan import AdaIN, GeneratorBlock
 from test.gans.test_util import tensor_equals
 
 
@@ -24,20 +24,114 @@ class AdaINTest(unittest.TestCase):
                 [98, 100, 102],
             ]
         ]])
-        y = AdaIN(x, torch.Tensor([-1,0,1]), torch.Tensor([10,100,100]))
+        y = AdaIN(x, torch.Tensor([-1, 0, 1]), torch.Tensor([10, 100, 100]))
 
-        self.assertTrue(y.shape == torch.Size((1,3,3,3)))
+        self.assertEquals(y.shape, torch.Size((1, 3, 3, 3)))
 
-        y_flattened = y.view(1,3,9)
+        y_flattened = y.view(1, 3, 9)
         y_mean = y_flattened.mean(dim=2, keepdim=True).view(3)
         y_std = y_flattened.std(dim=2, keepdim=True).view(3)
 
         self.assertTrue(tensor_equals(y_mean, torch.Tensor([-1, 0, 1])))
-        self.assertTrue(tensor_equals(y_std, torch.Tensor([10,100,100])))
+        self.assertTrue(tensor_equals(y_std, torch.Tensor([10, 100, 100])))
 
 
 class GeneratorBlockTests(unittest.TestCase):
-    pass
+    def test_upsample(self):
+        block = GeneratorBlock(in_channels=16, out_channels=16)
+        input_image = torch.ones(3, 16, 4, 4)
+
+        upsampled_image = block.upsample(input_image)
+
+        self.assertEquals(upsampled_image.shape, torch.Size((3, 16, 8, 8)))
+        self.assertEquals((upsampled_image - 1).abs().max(), 0.0)
+
+    def test_convolve_1(self):
+        block = GeneratorBlock(in_channels=16, out_channels=32)
+        input_image = torch.ones(3, 16, 8, 8)
+
+        conv_1_image = block.convolve_1(input_image)
+
+        self.assertEquals(conv_1_image.shape, torch.Size((3, 32, 8, 8)))
+
+    def test_create_noise__noise_input_is_none(self):
+        h = 256
+        w = 256
+
+        block = GeneratorBlock(in_channels=4, out_channels=4)
+        input_image = torch.ones(3, 4, h, w)
+
+        noise = block.create_noise(input_image, torch.Tensor([1, 2, 3, 4]).view(1, 4, 1, 1), None)
+
+        self.assertEquals(block.noise_1_factor.shape, torch.Size((1, 4, 1, 1)))
+        self.assertEquals(block.noise_2_factor.shape, torch.Size((1, 4, 1, 1)))
+        self.assertEquals(noise.shape, torch.Size((3, 4, h, w)))
+
+        std_diff = noise.view(3, 4, h * w).std(dim=2) - torch.Tensor([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]])
+        self.assertLess(std_diff.abs().max(), 0.1)
+
+        self.assertLess(noise.view(3, 4, h * w).mean(dim=2).abs().max(), 0.1)
+
+    def test_create_noise__noise_input_is_not_none(self):
+        h = 4
+        w = 4
+
+        block = GeneratorBlock(in_channels=4, out_channels=4)
+        input_image = torch.ones(3, 4, h, w)
+
+        noise = block.create_noise(
+            input_image,
+            noise_factor=torch.Tensor([1, 2, 3, 4]).view(1, 4, 1, 1),
+            input_noise=torch.Tensor([
+                [0.01, 0.02, 0.03, 0.04],
+                [0.1, 0.2, 0.3, 0.4],
+                [1, 2, 3, 4],
+                [10, 20, 30, 40]
+            ])
+        )
+
+        self.assertEquals(noise.shape, torch.Size((3, 4, h, w)))
+        self.assertTrue(tensor_equals(noise[0], torch.Tensor([
+            [
+                [0.01, 0.02, 0.03, 0.04],
+                [0.1, 0.2, 0.3, 0.4],
+                [1, 2, 3, 4],
+                [10, 20, 30, 40]
+            ],
+            [
+                [0.02, 0.04, 0.06, 0.08],
+                [0.2, 0.4, 0.6, 0.8],
+                [2, 4, 6, 8],
+                [20, 40, 60, 80]
+            ],
+            [
+                [0.03, 0.06, 0.09, 0.12],
+                [0.3, 0.6, 0.9, 1.2],
+                [3, 6, 9, 12],
+                [30, 60, 90, 120]
+            ],
+            [
+                [0.04, 0.08, 0.12, 0.16],
+                [0.4, 0.8, 1.2, 1.6],
+                [4, 8, 12, 16],
+                [40, 80, 120, 160]
+            ],
+        ])))
+        self.assertTrue(tensor_equals(noise[0], noise[1]))
+        self.assertTrue(tensor_equals(noise[0], noise[2]))
+
+    def test_forward(self):
+        h = 16
+        w = 16
+
+        block = GeneratorBlock(in_channels=32, out_channels=64)
+        input_image = torch.ones(3, 32, h, w)
+        weight = torch.ones(3, 512)
+
+        output_image = block.forward(input_image, weight)
+
+        self.assertEqual(output_image.shape, torch.Size((3, 64, 32, 32)))
+
 
 if __name__ == "__main__":
     unittest.main()
